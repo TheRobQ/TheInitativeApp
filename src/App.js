7b45d3b8bfc5fc479552a91b904e46ac42da5ebd
Robert Quan <rob.quan@gmail.com>
2018-05-11 12:21:30 -0600

    Removed app.css for troubleshooting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+// import './App.css';
 import Navbar from './components/Navbar';
 import LeftPanel from './components/LeftPanel';
 import MidPanel from './components/MidPanel';
