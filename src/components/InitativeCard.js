4d086c9b98a5132216f1df0913c62c4d45337364
Robert Quan <rob.quan@gmail.com>
2021-03-10 15:45:11 -0700

    Update InitativeCard.js

--- src/components/InitativeCard.js
+++ src/components/InitativeCard.js
@@ -1,10 +1,10 @@
 import React from 'react';
 // onClick={((event) => toggleRed(event))}
-const InitativeCard = ({name, modifier, highlight, removePlayer, id, turnRed}) =>{
+const Initiative = ({name, modifier, highlight, removePlayer, id, turnRed}) =>{
   let classes = (highlight === false) ? 'ordered' : 'ordered highlight'
   return(
     <p className={classes} id={id} onClick={((event) => turnRed(event, id))}>{name} {modifier}&nbsp; <button className='remove' onClick={((event) => removePlayer(event, id))}></button></p>
     )
 }
 
-export default InitativeCard
+export default Initiative
