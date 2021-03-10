16eb5922c773445379a72b3da14075e187a10195
Robert Quan <rob.quan@gmail.com>
2021-03-10 15:45:49 -0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = (props) =>  {
   return(
     <header className="header">
       <nav className="navbar navbar-dark bg-dark">
-        <span className="mb-0 display-4"><strong>Fight Night&nbsp;</strong><small className="text-muted"> &nbsp;A simple initiative tracker</small></span>
+        <span className="mb-0 display-4"><strong>Fight Night&nbsp;</strong><small className="text-muted"> &nbsp;A light-weight initiative tracker</small></span>
       </nav>
     </header>)
 }
